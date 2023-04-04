65b2461c5fb0da33e120097073fe471f2e08bf3d
nicolozibra1 <nico.zibra@gmail.com>
2023-04-04 16:46:58 +0200

    add-liveTime w/ (luxon)

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permett
 */
 
 // MYSCRIPT
-
+const DateTime = luxon.DateTime;
 const { createApp } = Vue;
 
 createApp({
@@ -34,17 +34,17 @@ createApp({
                 visible: false,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '15:30',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '15:50',
                         message: 'Ricordati di stendere i panni',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 16:15:22',
+                        date: '16:15',
                         message: 'Tutto fatto!',
                         status: 'received'
                     }
@@ -57,17 +57,17 @@ createApp({
                 visible: false,
                 messages: [
                     {
-                        date: '20/03/2020 16:30:00',
+                        date: '16:30',
                         message: 'Ciao come stai?',
                         status: 'sent'
                     },
                     {
-                        date: '20/03/2020 16:30:55',
+                        date: '16:30',
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received'
                     },
                     {
-                        date: '20/03/2020 16:35:00',
+                        date: '16:35',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent'
                     }
@@ -80,17 +80,17 @@ createApp({
                 visible: false,
                 messages: [
                     {
-                        date: '28/03/2020 10:10:40',
+                        date: '10:10',
                         message: 'La Marianna va in campagna',
                         status: 'received'
                     },
                     {
-                        date: '28/03/2020 10:20:10',
+                        date: '10:20',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
                     },
                     {
-                        date: '28/03/2020 16:15:22',
+                        date: '16:15',
                         message: 'Ah scusa!',
                         status: 'received'
                     }
@@ -103,12 +103,12 @@ createApp({
                 visible: false,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '15:30',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '15:50',
                         message: 'Si, ma preferirei andare al cinema',
                         status: 'received'
                     }
@@ -121,12 +121,12 @@ createApp({
                 visible: false,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '15:30',
                         message: 'Ricordati di chiamare la nonna',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '15:50',
                         message: 'Va bene, stasera la sento',
                         status: 'received'
                     }
@@ -139,17 +139,17 @@ createApp({
                 visible: false,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '15:30',
                         message: 'Ciao Claudia, hai novità?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '15:50',
                         message: 'Non ancora',
                         status: 'received'
                     },
                     {
-                        date: '10/01/2020 15:51:00',
+                        date: '15:51',
                         message: 'Nessuna nuova, buona nuova',
                         status: 'sent'
                     }
@@ -162,12 +162,12 @@ createApp({
                 visible: false,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '15:30',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '15:50',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
                         status: 'received'
                     }
@@ -180,17 +180,17 @@ createApp({
                 visible: false,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '15:30',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
                         status: 'received'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '15:50',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:51:00',
+                        date: '15:51',
                         message: 'OK!!',
                         status: 'received'
                     }
@@ -201,7 +201,8 @@ createApp({
             chatId: 0,
             textContent: '',
             textMessage: '',
-            response: null
+            response: null,
+            liveTime: null,
 
 
         }
@@ -229,8 +230,12 @@ createApp({
             }
         },
         sendMessage(){
+            const getCurrentTime = () => {
+                return DateTime.now().setLocale('it').toLocaleString(DateTime.TIME_SIMPLE);
+              };
+            this.liveTime = getCurrentTime()
             const newMessage = {
-                date: '',
+                date: this.liveTime,
                 message: this.textMessage,
                 status: 'sent'
             };
@@ -239,7 +244,7 @@ createApp({
             this.response = true;
             
             const botMessage = {
-                date: '',
+                date: this.liveTime,
                 message: 'Ok!',
                 status: 'received'
             };
@@ -252,7 +257,7 @@ createApp({
         
  },
     mounted() {
-
+        
     }
     
 }).mount('#app');
